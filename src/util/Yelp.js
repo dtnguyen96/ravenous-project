227d3815610b577d32219c1acf04d53ad8d0beb2
Dung Nguyen <dung34nguyen@gmail.com>
2020-06-01 10:35:03 -0500

    Create Yelp module

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,11 @@
+//Yelp crendentials
 const apiKey='SNVFhremgTZz5AuvJbElGo9uUhJC-kKq4vFMA2B-QKuBOnO8mfous_jXpoL6P1QSIKw-u8atj4rwHU9SVRKVzbNq6BE5femktlwIlrLPllqFnQaC_84BJFIkSn_UXnYx'
 const clientID='5miZqFIgWmhb85hEArfZsw'
+//Yelp module to store functinality needed to iteract with Yelp API
+const Yelp={
+    search(term, location, sortBy){
+        return fetch();
+    }
+}
+
+
