c99f82af1b7ae4e29f1df1836bb028ccb30c5268
Dung Nguyen <dung34nguyen@gmail.com>
2020-06-01 16:17:42 -0500

    fetch data and jsonify response

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -4,7 +4,27 @@ const clientID='5miZqFIgWmhb85hEArfZsw'
 //Yelp module to store functinality needed to iteract with Yelp API
 const Yelp={
     search(term, location, sortBy){
-        return fetch();
+        //Enable CORS Anywhere
+        //pass browser header into fetch
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+        {headers: {
+            Authorization: `Bearer ${apiKey}`}
+        //convert returned response into JSON 
+        }).then(response =>{
+            return response.json();
+        //Retrieve list of businesses from the converted JSON response
+        }).then(jsonResponse =>{
+        /*If response has a business key, return an array that has all of the business' properties:
+        name, address, city, etc.
+        If not, let site crash
+        */
+            if(jsonResponse.businesses){
+
+                return jsonResponse.businesses.map(business=>{
+                    
+                });
+            }
+        })
     }
 }
 
